bb9a5fc7dc234b570eba00be1db4ce0aad020665
cubiccm <lisongyuan0503@gmail.com>
2019-09-22 21:29:03 +0800

    Create potatojw_upgraded_pre.user.js

---
+++ potatojw_upgraded_pre.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Close potatojw alert
+// @namespace    https://cubiccm.ddns.net
+// @version      1.0
+// @description  土豆改善工程-pre
+// @author       potatojw_upgraded
+// @match        *://*.nju.edu.cn/jiaowu/student/index.do
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  alert = function() {};
+})();
