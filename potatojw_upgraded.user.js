10680b61daa7c9643fad054a1ed6b9a7b7ddd512
cubiccm <lisongyuan0503@gmail.com>
2019-09-23 16:52:36 +0800

    Update potatojw_upgraded.user.js
    v0.0.3
    +>适配通识课、通修课、阅读课选课
    ^>过滤器更新，增加课程名称过滤
    
    Note: 真的更新了很多很多，虽然日志只有区区两条

--- potatojw_upgraded.user.js
+++ potatojw_upgraded.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         potatojw_upgraded
 // @namespace    https://cubiccm.ddns.net
-// @version      0.0.2.1
+// @version      0.0.3
 // @description  土豆改善工程！
 // @author       Limosity
 // @match        *://*.nju.edu.cn/jiaowu/*
@@ -11,31 +11,32 @@
 (function() {
   'use strict';
   $$ = jQuery.noConflict();
-  console.log("potatojw_upgraded v0.0.2.1 by Limosity");
+  console.log("potatojw_upgraded v0.0.3 by Limosity");
   // Your code here...
   $$("head").append('<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">');
   var reg_gym = /gymClassList.do/i;
-  var reg_read = /readCourseList.do/i;
-  var reg_fr_dis = /freshman_discuss.do/i;
-  var reg_open = /open.do/i;
+  var reg_read = /readRenewCourseList.do/i;
+  var reg_dis = /discussRenewCourseList/i;
+  var reg_open = /openRenewCourseList/i;
+  var reg_common = /commonCourseRenewList/i;
 
   var mode = "";
-  if (reg_gym.test(window.location.href)) mode = "gym";
-  else if (reg_read.test(window.location.href)) mode = "read";
-  else if (reg_fr_dis.test(window.location.href)) mode = "fr_dis";
-  else if (reg_open.test(window.location.href)) mode = "open";
+  if (reg_gym.test(window.location.href)) mode = "gym"; //体育补选
+  else if (reg_read.test(window.location.href)) mode = "read"; // 经典导读读书班补选
+  else if (reg_dis.test(window.location.href)) mode = "dis"; // 导学、研讨、通识课补选
+  else if (reg_open.test(window.location.href)) mode = "open"; // 跨专业补选
+  else if (reg_common.test(window.location.href)) mode = "common"; //通修课补选
   else return;
 
   if (mode == "gym") {
+    // Submit gym class selection request
+    // 提交体育选课
     window.selectedClass = function(class_ID) {
-      document.getElementById('courseList').disabled = true;
-
       $$.ajax({
         url: "/jiaowu/student/elective/selectCourse.do",
         data: "method=addGymSelect&classId=" + class_ID,
         type: "POST",
         success: function(res) {
-          document.getElementById('courseList').disabled = false;
           $$("#courseOperation").css("display", "none");
           $$("#courseOperation").html(res);
           if ($$("#errMsg").length) {
@@ -45,8 +46,10 @@
           $$("#courseOperation").html("");
         }
       });
-    }
+    };
 
+    // Load gym class list
+    // 加载体育课列表
     window.initClassList = function(func = function() {}){
       $$.ajax({
         url: "/jiaowu/student/elective/courseList.do",
@@ -56,60 +59,26 @@
           $$("#courseList").html(res);
           updateFilterList();
           applyFilter();
+          if (auto_select_switch) doAutoClassSelect();
           func();
         }
       });
-    }
+    };
 
-    window.refreshCourseList = function() {
-      checkAvailableClass();
-    }
-
-    window.checkFilter = function (element) {
-      if ($$("#filter_switch").prop("checked") == false)
-        return true;
-      if ($$("#filter_full_class").prop("checked"))
-        if (parseInt($$(element).children("td:eq(3)").html()) >= parseInt($$(element).children("td:eq(4)").html()))
-          return false;
-      var current_time_val = $$(element).children("td:eq(1)").html();
-      if (date_list.indexOf(current_time_val) >= 0 && $$("#filter_time_checkbox_" + date_list.indexOf(current_time_val)).prop("checked") == false)
-        return false;
-      return true;
-    }
-
-    window.checkAvailableClass = function() {
-      initClassList(function() {
-        var allclass = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-        if (auto_select_switch)
-          $$("tbody > tr").each(function() {
-            if (!checkFilter(this)) return;
-            if (parseInt($$(this).children("td:eq(3)").html()) < parseInt($$(this).children("td:eq(4)").html())) {
-              $$(this).children("td:eq(5)").click();
-              console.log("Class Selected: " + i);
-            }
-          });
-      });
-    }
-    
-    window.startAutoCheck = function() {
-      var auto_check_times = 0, random_interval = 500 + Math.floor(Math.random() * 800);
-      return window.setInterval(function() {
-        window.setTimeout(function() {
-          checkAvailableClass();
-          console.log((++auto_check_times) + " times refreshed");
-        }, Math.floor(Math.random() * 1000));
-      }, random_interval);
-    }
+    // Check whether the class is full
+    // 检查课程是否满员
+    window.isClassFull = function(element) {
+      return parseInt($$(element).children("td:eq(3)").html()) >= parseInt($$(element).children("td:eq(4)").html());
+    };
   } else if (mode == "read") {
+    // Submit reading class selection request
+    // 提交阅读选课
     window.readSelect = function(event, class_ID, is_delete = false) {
-      $$("#courseList")[0].disabled = true;
-
       $$.ajax({
         url: "/jiaowu/student/elective/selectCourse.do",
         data: 'method=readCourse' + (is_delete ? 'Delete' : 'Select') + '&classid=' + class_ID,
         type: "POST",
         success: function(res) {
-          $$("#courseList")[0].disabled = false;
           $$("#courseDetail").html(res);
           $$('#courseOperation').html(res);
           if ($$("#errMsg").length == 0)
@@ -119,12 +88,17 @@
           readTypeChange();
         }
       });
-    }
+    };
 
-    window.initClassList = function(type = 1, success_func = function() {}){
+    // Load reading class list
+    // 加载阅读课列表
+    window.initClassList = function(success_func = function() {}){
+      var type;
+      if ($$('#readRenewType').length == 0) type = 7;
+      else type = $$('#readRenewType')[0].options[$$('#readRenewType')[0].selectedIndex].value;
       $$.ajax({
         url: "/jiaowu/student/elective/courseList.do",
-        data: 'method=readCourseList&type=' + type,
+        data: 'method=readRenewCourseList&type=' + type,
         type: "POST",
         success: function(res) {
           $$("#courseList").html(res);
@@ -132,18 +106,81 @@
           success_func();
         }
       });
-    }
+    };
 
-    var filter_type = 0;
+    // Detect reading type filter change
+    // 检测阅读类型过滤器更新
     window.readTypeChange = function() {
-      filter_type = $$('#readType')[0].options[$$('#readType')[0].selectedIndex].value;
-      initClassList(filter_type, hideCourseDetail);
-    }
+      initClassList(hideCourseDetail);
+    };
 
+    window.isClassFull = function(element) {
+      return parseInt($$(element).children("td:eq(5)").html()) >= parseInt($$(element).children("td:eq(4)").html());
+    };
+
+    // Drop a reading class
+    // 退选阅读课
     window.readDelete = function(event, class_ID) {
       readSelect(event, class_ID, true);
+    };
+  } else if (mode == "common") {
+    window.initClassList = function() {
+      $$.ajax({
+        url: window.location.href,
+        type: "GET",
+        success: function(res) {
+          $$("#tbCourseList").html($$(res).find("table").html());
+          updateFilterList();
+          applyFilter();
+        }
+      });
+    };
+
+    window.isClassFull = function(element) {
+      return parseInt($$(element).children("td:eq(7)").html()) >= parseInt($$(element).children("td:eq(6)").html());
+    };
+  } else if (mode == "dis") {
+    window.selectClass = function(class_ID) {
+      console.log(class_ID);
+      var g_campus = $$('#campusList')[0].options[$$('#campusList')[0].selectedIndex].value;
+      $$.ajax({
+        url: "/jiaowu/student/elective/courseList.do?method=submitDiscussRenew&classId=" + classId + "&campus=" + g_campus,
+        type: "GET",
+        success: function(res) {
+          console.log("Success!");
+          initClassList();
+        }
+      });
     }
 
+    window.optimizeClassList = function() {
+      $$("input[type='radio']").css("display", "none");
+      $$("input[type='button']").css("display", "none");
+      $$("input[type='radio']").each(function() {
+        $$(this).after("<a onclick='selectClass(" + $$(this).attr("value") + ");'>选择</a>")
+      });
+      updateFilterList();
+    }
+
+    $$(document).ready(function() {
+      optimizeClassList();
+    });
+
+    window.initClassList = function() {
+      $$.ajax({
+        url: window.location.href,
+        type: "GET",
+        success: function(res) {
+          $$("#tbCourseList").html($$(res).find("table").html());
+          optimizeClassList();
+          applyFilter();
+        }
+      });
+    };
+
+    window.isClassFull = function(element) {
+      return parseInt($$(element).children("td:eq(7)").html()) >= parseInt($$(element).children("td:eq(6)").html());
+    };
   } else return;
 
   const filter_setting_html = `
@@ -152,12 +189,22 @@
   <input type="checkbox" id="filter_full_class" checked="checked">
   <label for="filter_full_class">仅显示空余课程</label>
   <br>
+  <section id="filter_class_name">
+    <h3>课名过滤Beta</h3>
+    <h5>仅显示含有以下全部字符的课程（如：游泳 微积分）</h5>
+    <h5>说明：当且仅当下面输入框中的文字是课程名的连续一段文字时才会显示该课程~</h5>
+    <input type="text" id="filter_class_name_text">
+  </section>
+  <br>
   <section id="filter_time">
     <h3>仅显示这些上课时间：</h3>
   </section>
   <br>
   <button onclick="hideFilterSetting();">应用设置并关闭</button>
-  <span>potatojw_upgraded Beta v0.0.2.1 当前仅支持体育补选 注：自动选课是按过滤器选课~ 更多功能开发中~</span>
+  <br>
+  <span>potatojw_upgraded Beta v0.0.3 暂时不支持跨专业选课 注：自动选课是按过滤器选课~ 更多功能开发中~</span>
+  <br>
+  <span>字体美化已启用</span>
 </div>
   `;
   $$("body").append(filter_setting_html);
@@ -172,9 +219,9 @@
 <input type="checkbox" id="auto_select">
 <label for="auto_select">自动选课</label>
 <input type="checkbox" id="close_alert" disabled="disabled" checked="checked">
-<label for="close_alert">关闭所有提示框</label>
+<label for="close_alert">关闭选课提示框</label>
 <input type="checkbox" id="close_alert" disabled="disabled">
-<label for="close_alert">更漂亮的界面</label>
+<label for="close_alert">整体界面美化</label>
 <br>
 <span>potatojw_upgraded Beta</span>
 </div>
@@ -218,30 +265,22 @@
   border-radius: 8px;
   color: white;
   padding-left: 8px;
+  opacity: 0.8;
+}
+#filter_time {
+  display: none;
+}
+body {
+  font-family: 'PingFang SC', 'Microsoft YaHei', sans-serif;
 }
   `;
-  $$("body").append("<style>" + css + "</style>");
+  $$("body").append("<br><br><br><style>" + css + "</style>");
 
   window.applyFilter = function() {
     $$("tbody > tr").each(function() {
-      $$(this).css("display", (checkFilter(this) ? "table-row" : "none"));
+      $$(this).css("display", (filterClass(this) ? "table-row" : "none"));
     });
   };
-
-  window.date_list = new Array();
-  window.updateFilterList = function() {
-    var date_num = 0;
-    $$("tbody > tr").each(function() {
-      var current_time_val = $$(this).children("td:eq(1)").html();
-      if (date_list.includes(current_time_val)) return;
-      date_list.push(current_time_val);
-      var filter_time_append_html = `
-        <input type="checkbox" class="filter_time_checkbox" id="filter_time_checkbox_` + date_num + `" checked="checked">
-        <label for="filter_time_checkbox_` + (date_num++) + `">` + current_time_val + `</label><br>
-      `;
-      $$("section#filter_time").append(filter_time_append_html);
-    });
-  }
 
   window.showFilterSetting = function() {
     $$("#potatojw_mask").css("display", "block");
@@ -258,12 +297,12 @@
     applyFilter();
   });
 
-  window.check_id;
+  window.auto_refresh_interval_id;
   $$("#auto_refresh").change(function() {
     $$("#auto_refresh").prop("checked") ? (function() {
-      window.check_id = startAutoCheck();
+      window.auto_refresh_interval_id = startAutoRefresh();
     } ()) : (function() {
-      window.clearInterval(window.check_id);
+      window.clearInterval(window.auto_refresh_interval_id);
     } ());
   });
 
@@ -272,6 +311,107 @@
     if ($$("#auto_select").prop("checked") && !$$("#auto_refresh").prop("checked"))
       $$("#auto_refresh").click();
     window.auto_select_switch = $$("#auto_select").prop("checked");
-  })
+  });
+
+  const select_class_button_index = {
+    "gym": 5,
+    "read": 6,
+    "common": 9,
+    "dis": 10
+  };
+
+  const class_name_index = {
+    "gym": 0,
+    "read": 1,
+    "common": 2,
+    "dis": 2
+  };
+
+  const class_time_index = {
+    "gym": 1,
+    "common": 4,
+    "dis": 4
+  };
+
+  // Update class list automatically
+  // 自动更新
+  window.startAutoRefresh = function() {
+    var auto_check_times = 0, random_interval = 500 + Math.floor(Math.random() * 800);
+    return window.setInterval(function() {
+      window.setTimeout(function() {
+        initClassList();
+        console.log((++auto_check_times) + " times refreshed");
+      }, Math.floor(Math.random() * 1000));
+    }, random_interval);
+  };
+
+  // Select qualified class automatically
+  // 自动选择符合过滤器的课程
+  window.doAutoClassSelect = function() {
+    $$("tbody > tr").each(function() {
+      if (!filterClass(this)) return;
+      if (!isClassFull(this)) {
+        $$(this).children("td:eq(" + select_class_button_index[mode] + ")").children("a").click();
+        console.log("Class Selected: " + $$(this).children("td:eq(" + class_name_index[mode] + ")".html()));
+      }
+    });
+  };
+
+  // Get the time of a given class
+  // 获取课程上课时间
+  window.getClassTime = function(element) {
+    return $$(element).children("td:eq(" + class_time_index[mode] + ")").html();
+  };
+
+  window.time_list = new Array();
+  window.updateFilterList = function() {
+    var date_num = 0;
+    $$("tbody > tr").each(function() {
+      if (typeof(class_time_index[mode]) != "undefined") {
+        var current_time_val = getClassTime(this);
+        var str_array = current_time_val.split("<br>");
+        for (var i = 0; i < str_array.length; i++) {
+          if (time_list.includes(str_array[i])) return;
+          time_list.push(str_array[i]);
+          var filter_time_append_html = `
+            <input type="checkbox" class="filter_time_checkbox" id="filter_time_checkbox_` + date_num + `" checked="checked">
+            <label for="filter_time_checkbox_` + (date_num++) + `">` + str_array[i] + `</label><br>
+          `;
+          $$("section#filter_time").append(filter_time_append_html);
+        }
+      }
+    });
+  };
+  updateFilterList();
+
+  if (typeof(class_time_index[mode]) != "undefined")
+    $$("section#filter_time").css("display", "block");
+
+  // Check if the given class satisfy the filter
+  // 检查课程是否符合过滤器
+  window.filterClass = function(element) {
+    if ($$("#filter_switch").prop("checked") == false)
+      return true;
+    if ($$("#filter_full_class").prop("checked"))
+      if (isClassFull(element))
+        return false;
+    if (typeof(class_time_index[mode]) != "undefined") {
+      var current_time_val = getClassTime(element);
+      var str_array = current_time_val.split("<br>");
+      for (var i = 0; i < str_array.length; i++)
+        if (time_list.indexOf(str_array[i]) >= 0 && $$("#filter_time_checkbox_" + time_list.indexOf(str_array[i])).prop("checked") == false)
+          return false;
+    }
+    var current_class_name = $$(element).children("td:eq(" + class_name_index[mode] + ")").html();
+    if (current_class_name.indexOf($$("#filter_class_name_text").val()) < 0)
+      return false;
+    return true;
+  };
+
+  // Rewrite refresh function
+  // 改写刷新按钮：刷新课程列表
+  window.refreshCourseList = function() {
+    initClassList();
+  };
 
 })();
