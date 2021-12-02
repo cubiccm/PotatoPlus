60970fea0b283a2b98ce0fb956b8ba92ef07ac51
Limosity <illimosity@gmail.com>
2021-12-02 17:28:58 +0800

    Update pjw-lib.js

--- js/pjw-lib.js
+++ js/pjw-lib.js
@@ -18,46 +18,46 @@ var options_id = {
   "read": "readRenewType"
 };
 
-var atw_lyrics = `
-I walked through the door with you, the air was cold
+var atw_lyrics = `I walked through the door with you, the air was cold
 But something 'bout it felt like home somehow
 And I left my scarf there at your sister's house
 And you've still got it in your drawer, even now
-Oh, your sweet disposition and my wide-eyed gaze
+Oh your sweet disposition, and my wide-eyed gaze
 We're singing in the car, getting lost upstate
 Autumn leaves falling down like pieces into place
 And I can picture it after all these days
 And I know it's long gone and that magic's not here no more
 And I might be okay, but I'm not fine at all
-'Cause there we are again, on that little town street
+'Cause there we are again on that little town street
 You almost ran the red 'cause you were looking over at me
 Wind in my hair, I was there
 I remember it all too well
 Photo album on the counter, your cheeks were turning red
 You used to be a little kid with glasses in a twin-sized bed
 And your mother's telling stories 'bout you on the tee-ball team
-You taught me 'bout your past thinking your future was me
+You taught me 'bout your past thinkin' your future was me
 And you were tossing me the car keys
-Fuck the patriarchy
-Keychain on the ground, we were always skipping town
-And I was thinking on the drive down, "Any time now
-He's gonna say it's love", you never called it what it was
+'Fuck the patriarchy' keychain on the ground
+We were always skipping town
+And I was thinking on the drive down
+Any time now, he's gonna say it's love
+You never called it what it was
 'Til we were dead and gone and buried
 Check the pulse and come back swearing it's the same
-After three months in the grave
-And then you wondered where it went to as I reached for you
-But all I felt was shame, and you held my lifeless frame
+After 3 months in the grave
+And then you wondered where it went to as I reached for you, but all I felt was shame
+And you held my lifeless frame
 And I know it's long gone and there was nothing else I could do
 And I forget about you long enough to forget why I needed to
-'Cause there we are again, in the middle of the night
-We're dancing 'round the kitchen in the refrigerator light
+'Cause there we are again in the middle of the night
+We're dancin' 'round the kitchen in the refrigerator light
 Down the stairs, I was there
 I remember it all too well
 And there we are again when nobody had to know
 You kept me like a secret, but I kept you like an oath
 Sacred prayer and we'd swear
-To remember it all too well, yeah
-And maybe we got lost in translation, maybe I asked for too much
+To remember it all too well
+Well maybe we got lost in translation, maybe I asked for too much
 But maybe this thing was a masterpiece 'til you tore it all up
 Running scared, I was there
 I remember it all too well
@@ -65,22 +65,21 @@ And you call me up again just to break me like a promise
 So casually cruel in the name of being honest
 I'm a crumpled up piece of paper lying here
 'Cause I remember it all, all, all
-They say, "All's well that ends well" but I'm in a new hell every time
+They say all's well that ends well, but I'm in a new hell every time
 You double-cross my mind
-You said, "If we had been closer in age, maybe it would've been fine"
+You said if we had been closer in age, maybe it would've been fine
 And that made me want to die
 The idea you had of me, who was she?
-A never-needy, ever lovely jewel whose shine reflects on you
+A never-needy, ever lovely jewel, whose shine reflects on you
 Not weeping in a party bathroom
 Some actress asking me what happened, you
-That's what happened, you
-You who charmed my dad with self-effacing jokes
-Sipping coffee like you're on a late-night show
+That's what happened: you
+You, who charmed my dad with self-effacing jokes
+Sipping coffee like you were on a late-night show
 Then he watched me watch the front door all night, willing you to come
-And he said, "It's supposed to be fun turning 21"
+And he said, "it's supposed to be fun... turning 21"
 Time won't fly, it's like I'm paralyzed by it
-I'd like to be my old self again
-But I'm still trying to find it
+I'd like to be my old self again, but I'm still trying to find it
 After plaid shirt days and nights when you made me your own
 Now you mail back my things and I walk home alone
 But you keep my old scarf from that very first week
@@ -93,17 +92,18 @@ It was rare, I was there, I remember it all too well
 Wind in my hair, you were there, you remember it all
 Down the stairs, you were there, you remember it all
 It was rare, I was there, I remember it all too well
-And I was never good at telling jokes, but the punchline goes
-"I'll get older, but your lovers stay my age"
+And I was never good at telling jokes
+But the punchline goes: "I'll get older, but your lovers stay my age"
 From when your Brooklyn broke my skin and bones
 I'm a soldier who's returning half her weight
 And did the twin flame bruise paint you blue?
 Just between us, did the love affair maim you too?
-'Cause in this city's barren cold, I still remember the first fall of snow
+'Cause in this city's barren cold
+I still remember the first fall of snow
 And how it glistened as it fell
 I remember it all too well
-Just between us, did the love affair maim you all too well?
-Just between us, do you remember it all too well?
+Just between us, did the love affair maim you all too well
+Just between us, do you remember it all too well
 Just between us, I remember it all too well
 Wind in my hair, I was there, I was there
 Down the stairs, I was there, I was there
@@ -112,7 +112,11 @@ It was rare, you remember it
 Wind in my hair, I was there, I was there
 Down the stairs, I was there, I was there
 Sacred prayer, I was there, I was there
-It was rare, you remember it
+It was rare, you remember it (all too well)
+Wind in my hair, I was there, I was there
+Down the stairs, I was there, I was there
+Sacred prayer, I was there, I was there
+It was rare, you remember it (all too well)
 Wind in my hair, I was there, I was there
 Down the stairs, I was there, I was there
 Sacred prayer, I was there, I was there
@@ -123,8 +127,7 @@ Sacred prayer, I was there, I was there
 It was rare, you remember it...
 `;
 
-var frozen_quotes = `
-The snow glows white on the mountain tonight, not a footprint to be seen.
+var frozen_quotes = `The snow glows white on the mountain tonight, not a footprint to be seen.
 A kingdom of isolation, and it looks like I'm the queen.
 The wind is howling like this swirling storm inside.
 Don't let them in, don't let them see, be the good girl you always have to be
